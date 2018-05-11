ffa3166464bb263edd4487fd3e7259052de36fdd
舒岩 <shuyan434224591@gmail.com>
2018-05-11 10:32:50 +0800

    学习完查询和添加

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # mybatis(原生)
-## 1. 目录结构及文件介绍
+## 1. 配置文件介绍
 + SqlMapConfig.xml<br/>
    >mybatis全局配置文件<br/>
     用于配置数据库驱动、连接、账号、密码等信息
@@ -9,3 +9,69 @@
     在该文件中配置sql语句<br/>
     文件名不固定是`mapper.xml`，这只是个统称，名称可以是：`User.xml`
     
+## 2. mybatis使用步骤
+1. 导入jar包
+2. 在resources目录下创建配置文件<br/>
+    + 创建`SqlMapConfig`文件，并加入如下内容
+        ```xml
+        <?xml version="1.0" encoding="UTF-8" ?>
+        <!DOCTYPE configuration
+                PUBLIC "-//mybatis.org//DTD Config 3.0//EN"
+                "http://mybatis.org/dtd/mybatis-3-config.dtd">
+        <configuration>
+            <!-- 和spring整合后 environments配置将废除-->
+            <environments default="development">
+                <environment id="development">
+                    <!-- 使用jdbc事务管理-->
+                    <transactionManager type="JDBC"/>
+                    <!-- 数据库连接池，由mybatis管理-->
+                    <dataSource type="POOLED">
+                        <property name="driver" value="com.mysql.jdbc.Driver"/>
+                        <property name="url" value="jdbc:mysql://localhost:3306/test1?characterEncoding=utf-8"/>
+                        <property name="username" value="root"/>
+                        <property name="password" value="root"/>
+                    </dataSource>
+                </environment>
+            </environments>
+        </configuration>
+        ```
+    + 在`resources/sqlmap`下创建`mapper.xml`文件，加入如下内容：
+        ```xml
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE mapper
+              PUBLIC "-//mybatis.org//DTD Mapper 3.0//EN"
+              "http://mybatis.org/dtd/mybatis-3-mapper.dtd">
+        
+        <!-- 通过命令空间对sql进行分类管理 -->
+        <mapper namespace="test">
+          
+        </mapper>
+        ```
+    + __注意__<br/>
+        本程序中的`resources`目录就是`config`目录，
+        将`config`目录右键 -> Mark Directory as -> Resources Root后，
+        在程序中才能找到他下面的配置文件
+3. 编写各表的PO类（javabean）
+4. 编写mapper.xml文件<br/>
+    在mapper标签中配置sql语句
+    如：
+    ```xml
+    <select id="findUserById" parameterType="int" resultType="com.shuyan.po.User">
+        select * from user where id = #{id}
+    </select>
+    ```
+5. 在`SqlMapConfig.xml`中加载`mapper.xml`
+    ```xml
+    <mappers>
+        <mapper resource="sqlmap/User.xml"/>
+    </mappers>
+    ```
+6. 编写java代码<br/>
+## 3. 知识点
+1. 原生mybatis自动提交事务是关闭的，所以对数据库内容进行更改的操作需要手动提交事务
+
+## 问题
+1. 配置mapper时，输入参数类型只能指定1种，但是如果需要输入多个不同类型的参数时，该如何处理
+2. 向sql中传入参数时不需要指定参数名称，传入多个参数时如何处理
+
+
---
+++ src/com/shuyan/demo1/UserTest.java
@@ -0,0 +1,88 @@
+package com.shuyan.demo1;
+
+import com.shuyan.po.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
+
+public class UserTest {
+    @Test
+    public void findUserById() throws IOException {
+        //读取SqlMapConfig.xml全局配置文件，文件名基于resources目录
+        InputStream inputStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        //创建回话工厂
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        //通过工程得到sqlsession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        /*
+         * 通过sqlsession执行sql语句
+         * selectOne:查询1条记录
+         * 参数：
+         *      参数1：命名空间+.+statementID,通过命名空间和statementID就可以找到要执行的sql
+         *      参数2：输入参数，对应该sql中的占位符，参数类型对应该sql的parameterType
+         * 返回值：
+         *      PO类对象，类型对应该sql的resultType
+         * 注意：
+         *      当查询结果为多条记录时，不能使用selectOne进行查询
+         */
+        User user = sqlSession.selectOne("test.findUserById", 1);
+        System.out.println(user);
+        //关闭sqlsession
+        sqlSession.close();
+    }
+
+    @Test
+    public void findUserByUsernameLike() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        /*
+         * selectList：查询多条记录
+         */
+        List<User> list = sqlSession.selectList("test.findUserByUsernameLike", "小明");
+        for (User user: list) {
+            System.out.println(user);
+        }
+        sqlSession.close();
+    }
+
+    @Test
+    public void insertUser() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        User user = new User("小红",new Date(),"男","立水桥");
+        /*
+         * insert：插入数据
+         */
+        sqlSession.insert("test.insertUser", user);
+        //对数据库有更改的操作需要提交事务
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    public void insertUserAndGetId() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        User user = new User("小蓝",new Date(),"男","分钟寺");
+        /*
+         * insert：插入数据
+         */
+        sqlSession.insert("test.insertUserAndGetId", user);
+        //对数据库有更改的操作需要提交事务
+        sqlSession.commit();
+        //因为插入操作后执行了查询主键并保存到user对象中的操作，所以这里user对象的id就是插入后返回的id
+        //User{id=29, username='小蓝', birthday=Fri May 11 10:11:01 CST 2018, sex='男', address='分钟寺'}
+        System.out.println(user);
+        sqlSession.close();
+    }
+}
