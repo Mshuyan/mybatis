7c1d8ea31146f81ee5f1424bfa1e8dfd045277a7
Will <shuyan434224591@gmail.com>
2019-02-22 17:56:52 +0800

    no message

--- mybatis-redis/src/test/java/com/shuyan/mybatis/mybatisredis/MybatisRedisApplicationTests.java
+++ mybatis-redis/src/test/java/com/shuyan/mybatis/mybatisredis/MybatisRedisApplicationTests.java
@@ -2,6 +2,7 @@ package com.shuyan.mybatis.mybatisredis;
 
 import com.shuyan.mybatis.mybatisredis.business.bean.RegionEntity;
 import com.shuyan.mybatis.mybatisredis.business.mapper.RegionMapper;
+import lombok.Synchronized;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
